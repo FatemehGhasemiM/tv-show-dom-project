8b8cf9f2367ec77e2d2f6f9cba7d446f679c7265
Fatemeh <fatemehghasemi9900@gmail.com>
2023-01-04 03:20:04 +0330

    edit styles

--- script.js
+++ script.js
@@ -1,37 +1,106 @@
 const container = document.querySelector(".container");
+const header = document.querySelector("header");
 const search = document.getElementById("search");
+const btn = document.querySelector("button");
+const subject = [];
 
-const createElement = (data) => {
+// create card
+const createElement = (el) => {
+  const section = document.createElement("section");
+  const img = document.createElement("img");
+  let title = document.createElement("h2");
+  let summary = document.createElement("section");
+
+  section.setAttribute("id", el.id);
+  img.setAttribute("src", el.image.medium);
+  summary.innerHTML = el.summary;
+
+  // episode and season
+  el.season > 9 && el.number > 9
+    ? (title.textContent = `${el.name} - S${el.season}E${el.number}`)
+    : (title.textContent = `${el.name} - S0${el.season}E0${el.number}`);
+  el.season > 9 && el.number < 10
+    ? (title.textContent = `${el.name} - S${el.season}E0${el.number}`)
+    : (title.textContent = `${el.name} - S0${el.season}E${el.number}`);
+
+  subject.push(title.textContent);
+
+  // add elements
+  container.append(section);
+  section.append(img, title, summary);
+  title.style.paddingTop = "15px";
+  title.style.paddingBottom = "15px";
+  section.classList.add("card");
+  summary.classList.add("summary");
+};
+
+// display every card
+const showTv = (data) => {
   data.forEach((el) => {
-    const section = document.createElement("section");
-    const img = document.createElement("img");
-    let title = document.createElement("h2");
-    let summary = document.createElement("section");
-    img.setAttribute("src", el.image.medium);
-    summary.innerHTML = el.summary;
-
-    // order episode and season accordingly
-    el.season > 9 && el.number > 9
-      ? (title.textContent = `${el.name} - S${el.season}E${el.number}`)
-      : (title.textContent = `${el.name} - S0${el.season}E0${el.number}`);
-    el.season > 9 && el.number < 10
-      ? (title.textContent = `${el.name} - S${el.season}E0${el.number}`)
-      : (title.textContent = `${el.name} - S0${el.season}E${el.number}`);
-
-    // add elements
-    container.append(section);
-    section.append(img, title, summary);
-    title.style.paddingTop = "15px";
-    title.style.paddingBottom = "15px";
-    section.classList.add("card");
-    summary.classList.add("summary");
+    createElement(el);
   });
 };
 
+let data = [];
 async function getData(url) {
   const response = await fetch(url);
-  const data = await response.json();
-  createElement(data);
+  data = await response.json();
+  showTv(data);
+  selectCharacters(data);
+
+  // search in cards
+  search.addEventListener("keyup", (e) => {
+    const searchTerm = e.target.value.toUpperCase();
+    let dataSearch = [];
+    data.map((p) => {
+      if (
+        p.name.toUpperCase().includes(searchTerm) ||
+        p.summary.toUpperCase().includes(searchTerm)
+      ) {
+        dataSearch.push(p);
+      }
+      container.innerHTML = "";
+      showTv(dataSearch);
+    });
+    if (!search.value) {
+      container.innerHTML = "";
+      showTv(data);
+    }
+    console.log(e.target.value.toUpperCase());
+  });
 }
 
+// select option
+const select = document.createElement("select");
+function selectCharacters() {
+  for (webpage of data) {
+    select.classList.add("select");
+    const option = document.createElement("option");
+    option.innerHTML = `S0${webpage.season}E0${webpage.number} - ${webpage.name}`;
+    option.classList = "option";
+    select.appendChild(option);
+  }
+}
+header.appendChild(select);
+select.addEventListener("change", () => {
+  search.value = "";
+  const option = document.getElementsByClassName("option");
+  const box = document.getElementsByClassName("card");
+  let selects1 = select.options[select.selectedIndex].value;
+
+  for (let i = 0; i < select.length; i++) {
+    if (option[i].value === selects1) {
+      box[i].style.display = "block";
+    } else {
+      box[i].style.display = "none";
+    }
+  }
+  box.style.overflow = "hidden";
+});
+
+btn.addEventListener("click", () => {
+  search.value = "";
+  container.innerHTML = "";
+  showTv(data);
+});
 getData("https://api.tvmaze.com/shows/22036/episodes");
